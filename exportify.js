d6e9e089258fbe14033e849e2297a295be385ef3
Pavel Komarov <pvlkmrv@gmail.com>
2019-06-22 15:38:41 -0400

    big changes. Now relying on fetch instead of jquery's ajax function. Much more explicit about promises and how returned data flows. Took out all the special case handling for the starred playlist, since you can't even download that one anyway. Added plenty of comments, including some about how React works. I like it. The core functionality is now broken, because somehow the changes don't agree with the PlaylistExporter, but at least now we're not relying on arguments[0] that seems to come out of thin air.

--- exportify.js
+++ exportify.js
@@ -1,7 +1,5 @@
-var State = {} // a place to keep track of program state. Gets reset every time the script is fully reloaded.
-
-function print(obj) {
-  console.log(JSON.stringify(obj, null, 2))
+function print(label, obj) {
+  console.log(label + JSON.stringify(obj, null, 2))
 }
 
 // A collection of functions to create and send API queries
@@ -18,26 +16,17 @@ utils = {
       "&response_type=token";
   },
 
-  // Make an asynchronous call to the server.
-  // https://api.jquery.com/jquery.ajax/
+  // Make an asynchronous call to the server. Promises are *wierd*. Careful here! You have to call .json() on the
+  // promise returned by the fetch to get a second promise that has the actual data in it!
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   apiCall(url, access_token) {
-    return $.ajax({
-      url: url,
-      headers: {
-        'Authorization': 'Bearer ' + access_token
-      }
-    }).fail(function (jqXHR, textStatus) {
-      if (jqXHR.status == 401) {
-        // Return to home page after auth token expiry
-        window.location = window.location.href.split('#')[0]
-      } else if (jqXHR.status == 429) {
-        // API Rate-limiting encountered
-        $('#rateLimitMessage').show()
-      } else {
-        // Otherwise report the error so user can raise an issue
-        alert(jqXHR.responseText);
-      }
-    })
+    let promise = fetch(url, { headers: { 'Authorization': 'Bearer ' + access_token} });
+    return promise.then(response => {
+      if (response.ok || response.status == 304) { print("response code", response.status); return response.json(); }
+      else if (response.status == 401) { window.location = window.location.href.split('#')[0]; } // Return to home page after auth token expiry
+      else if (response.status == 429) { $('#rateLimitMessage').show(); } // API Rate-limiting encountered
+      else { alert(response.status); }
+    });
   }
 }
 
@@ -54,163 +43,124 @@ let PlaylistTable = React.createClass({
     };
   },
 
+  // "componentDidMount() is invoked immediately after a component is mounted (inserted into the tree).
+  // Initialization that requires DOM nodes should go here."
+  componentDidMount() {
+    this.loadPlaylists(this.props.url);
+  }, 
+
+  // Retrieve and display the list of user playlists. There are three steps: (1) retrieve data about the user,
+  // (2) wait for it to come back, then use it to ask for the list of playlists, (3) wait for that to come back,
+  // then parse that information out in to the React table.
   loadPlaylists(url) {
-    let userId = '';
-    let firstPage = typeof url === 'undefined' || url.indexOf('offset=0') > -1;
-
-    utils.apiCall("https://api.spotify.com/v1/me", this.props.access_token).then(function(response) {
-      userId = response.id;
-
-      // Show starred playlist if viewing first page
-      if (firstPage) {
-        return $.when.apply($, [
-          utils.apiCall(
-            "https://api.spotify.com/v1/users/" + userId + "/starred",
-            this.props.access_token
-          ),
-          utils.apiCall(
-            "https://api.spotify.com/v1/users/" + userId + "/playlists",
-            this.props.access_token
-          )
-        ])
-      } else {
-        return utils.apiCall(url, this.props.access_token);
-      }
-    }.bind(this)).done(function() {//God, why, JavaScript? Why? These crazy chained constructions
-      let response;
-      let playlists = [];
-
-      if (arguments[1] === 'success') {
-        response = arguments[0];
-        playlists = arguments[0].items;
-      } else {
-        response = arguments[1][0];
-        playlists = $.merge([arguments[0][0]], arguments[1][0].items);
-      }
-
+    let promise = utils.apiCall("https://api.spotify.com/v1/me", this.props.access_token);
+    promise = promise.then(data => {
+      return utils.apiCall("https://api.spotify.com/v1/users/" + data.id + "/playlists", this.props.access_token)
+    });
+    promise = promise.then(response => {
       if (this.isMounted()) {
         this.setState({
-          playlists: playlists,
+          playlists: response.items,
           playlistCount: response.total,
           nextURL: response.next,
           prevURL: response.previous
         });
 
         $('#playlists').fadeIn();
-        $('#subtitle').text((response.offset + 1) + '-' + (response.offset + response.items.length) + ' of ' + response.total + ' playlists for ' + userId)
+        $('#subtitle').text((response.offset + 1) + '-' +
+          (response.offset + response.items.length) + ' of ' + response.total + ' playlists')
       }
-    }.bind(this))
+    });
   },
 
   exportPlaylists() {
     PlaylistsExporter.export(this.props.access_token, this.state.playlistCount);
   },
 
-  componentDidMount() {
-    this.loadPlaylists(this.props.url);
-  },
-
+  // This is the power of React. You get to use this "JSX Syntax" to define and configure html really easily.
+  // {} are escape characters that demarcate javascript that needs to be evaluated to complete the html.
+  // This is much like Jekyll's Liquid preprocessing engine.
   render() {
-    if (this.state.playlists.length > 0) {
-      return (
-        <div id="playlists">
-          <Paginator nextURL={this.state.nextURL} prevURL={this.state.prevURL} loadPlaylists={this.loadPlaylists}/>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th style={{width: "30px"}}></th>
-                <th>Name</th>
-                <th style={{width: "150px"}}>Owner</th>
-                <th style={{width: "100px"}}>Tracks</th>
-                <th style={{width: "120px"}}>Public?</th>
-                <th style={{width: "120px"}}>Collaborative?</th>
-                <th style={{width: "100px"}} className="text-right">
-                <button className="btn btn-default btn-xs" type="submit" onClick={this.exportPlaylists}>
-                <span className="fa fa-file-archive-o"></span> Export All</button></th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.playlists.map(function(playlist, i) {
-                return <PlaylistRow playlist={playlist} key={playlist.id} access_token={this.props.access_token}/>;
-              }.bind(this))}
-            </tbody>
-          </table>
-          <Paginator nextURL={this.state.nextURL} prevURL={this.state.prevURL} loadPlaylists={this.loadPlaylists}/>
-        </div>
-      );
-    } else {
-      return <div className="spinner"></div>
-    }
+    return (
+      <div id="playlists">
+        <Paginator nextURL={this.state.nextURL} prevURL={this.state.prevURL} loadPlaylists={this.loadPlaylists}/>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th style={{width: "30px"}}></th>
+              <th>Name</th>
+              <th style={{width: "150px"}}>Owner</th>
+              <th style={{width: "100px"}}>Tracks</th>
+              <th style={{width: "120px"}}>Public?</th>
+              <th style={{width: "120px"}}>Collaborative?</th>
+              <th style={{width: "100px"}} className="text-right">
+              <button className="btn btn-default btn-xs" type="submit" onClick={this.exportPlaylists}>
+              <span className="fa fa-file-archive-o"></span> Export All</button></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.playlists.map((playlist, i) => {
+              return <PlaylistRow playlist={playlist} access_token={this.props.access_token}/>;
+            })}
+          </tbody>
+        </table>
+        <Paginator nextURL={this.state.nextURL} prevURL={this.state.prevURL} loadPlaylists={this.loadPlaylists}/>
+      </div>
+    );
   }
 });
 
+// Separated out for convenience, I guess. The table's render method defines a bunch of these in a loop, which I'm
+// guessing implicitly calls this thing's render method. 
 let PlaylistRow = React.createClass({
-  exportPlaylist: function() {
+  exportPlaylist() { // this is the function that gets called when an export button is pressed
     PlaylistExporter.export(this.props.access_token, this.props.playlist);
   },
 
-  renderTickCross: function(condition) {
-    if (condition) {
+  renderTickCross(dark) {
+    if (dark) {
       return <i className="fa fa-lg fa-check-circle-o"></i>
     } else {
-      return <i className="fa fa-lg fa-times-circle-o" style={{ color: '#ECEBE8' }}></i>
+      return <i className="fa fa-lg fa-times-circle-o" style={{color: '#ECEBE8'}}></i>
     }
   },
 
-  renderIcon: function(playlist) {
-    if (playlist.name == 'Starred') {
-      return <i className="glyphicon glyphicon-star" style={{ color: 'gold' }}></i>;
-    } else {
-      return <i className="fa fa-music"></i>;
-    }
+  renderIcon(playlist) {
+    return <i className="fa fa-music"></i>;
   },
 
-  render: function() {
-    playlist = this.props.playlist
-    if(playlist.uri==null) return (
-      <tr key={this.props.key}>
-        <td>{this.renderIcon(playlist)}</td>
-        <td>{playlist.name}</td>
-        <td colSpan="2">This playlist is not supported</td>
-        <td>{this.renderTickCross(playlist.public)}</td>
-        <td>{this.renderTickCross(playlist.collaborative)}</td>
-        <td>&nbsp;</td>
-      </tr>
-    );
-    return (
-      <tr key={this.props.key}>
-        <td>{this.renderIcon(playlist)}</td>
-        <td><a href={playlist.uri}>{playlist.name}</a></td>
-        <td><a href={playlist.owner.uri}>{playlist.owner.id}</a></td>
-        <td>{playlist.tracks.total}</td>
-        <td>{this.renderTickCross(playlist.public)}</td>
-        <td>{this.renderTickCross(playlist.collaborative)}</td>
-        <td className="text-right"><button className="btn btn-default btn-xs btn-success" type="submit" onClick={this.exportPlaylist}><span className="glyphicon glyphicon-save"></span> Export</button></td>
+  render() {
+    p = this.props.playlist
+    return ( // actual html for one row of the table
+      <tr key={p.id}>
+        <td>{this.renderIcon(p)}</td>
+        <td><a href={p.uri}>{p.name}</a></td>
+        <td><a href={p.owner.uri}>{p.owner.id}</a></td>
+        <td>{p.tracks.total}</td>
+        <td>{this.renderTickCross(p.public)}</td>
+        <td>{this.renderTickCross(p.collaborative)}</td>
+        <td className="text-right"><button className="btn btn-default btn-xs btn-success" type="submit"
+          onClick={this.exportPlaylist}><span className="glyphicon glyphicon-save"></span> Export</button></td>
       </tr>
     );
   }
 });
 
+// For those users with a lot more playlists than necessary
 let Paginator = React.createClass({
-  nextClick: function(e) {
+  nextClick(e) {
     e.preventDefault()
-
-    if (this.props.nextURL != null) {
-      this.props.loadPlaylists(this.props.nextURL)
-    }
+    if (this.props.nextURL != null) { this.props.loadPlaylists(this.props.nextURL); }
   },
 
-  prevClick: function(e) {
+  prevClick(e) {
     e.preventDefault()
-
-    if (this.props.prevURL != null) {
-      this.props.loadPlaylists(this.props.prevURL)
-    }
+    if (this.props.prevURL != null) { this.props.loadPlaylists(this.props.prevURL); }
   },
 
-  render: function() {
-    if (this.props.nextURL != null || this.props.prevURL != null) {
-      return (
+  render() {
+    if (!this.props.nextURL && !this.props.prevURL) { return <div>&nbsp;</div> }
+    else { return (
         <nav className="paginator text-right">
           <ul className="pagination pagination-sm">
             <li className={this.props.prevURL == null ? 'disabled' : ''}>
@@ -225,14 +175,13 @@ let Paginator = React.createClass({
             </li>
           </ul>
         </nav>
-      )
-    } else {
-      return <div>&nbsp;</div>
-    }
+      ); }
   }
 });
 
 // Handles exporting all playlist data as a zip file
+// Skipping Pavelizing for right now, but this thing is certainly much too long and probably broken after my other
+// changes. Needs some attention, but I'm not as interested in this use case.
 let PlaylistsExporter = {
   export(access_token, playlistCount) {
     let playlistFileNames = [];
@@ -299,22 +248,24 @@ let PlaylistsExporter = {
 
 // Handles exporting a single playlist as a CSV file
 let PlaylistExporter = {
+  // Take the access token string and playlist object, generate a csv from it, and when that data is resolved and
+  // returned save to a file.
   export(access_token, playlist) {
-    this.csvData(access_token, playlist).then(function(data) {
-      let blob = new Blob(["\uFEFF" + data], { type: "text/csv;charset=utf-8" });
-      saveAs(blob, this.fileName(playlist));
-    }.bind(this))
+    let promise = this.csvData(access_token, playlist);
+    let fileName = this.fileName(playlist);
+    promise.then((data) => { saveAs(new Blob(["\uFEFF" + data], { type: "text/csv;charset=utf-8" }), fileName) });
   },
 
-  csvData: function(access_token, playlist) {
+  // This is where the magic happens. The access token gives us permission to query this info from Spotify, and the
+  // playlist object gives us all the information we need to start asking for songs.
+  csvData(access_token, playlist) {
+    // Make asynchronous API calls for 100 songs at a time, and put the results in a list.
     let requests = [];
-    let limit = 100;
-
-    for (let offset = 0; offset < playlist.tracks.total; offset = offset + limit) {
-      requests.push(
-        utils.apiCall(playlist.tracks.href.split('?')[0] + '?offset=' + offset + '&limit=' + limit, access_token)
-      )
+    for (let offset = 0; offset < playlist.tracks.total; offset = offset + 100) {
+      requests.push(utils.apiCall(playlist.tracks.href.split('?')[0] + '?offset=' + offset + '&limit=100',
+          access_token));
     }
+
 
     return $.when.apply($, requests).then(function() {
       let responses = [];
@@ -373,28 +324,31 @@ let PlaylistExporter = {
     });
   },
 
-  fileName: function(playlist) {
+  // take the playlist object and return an acceptable filename
+  fileName(playlist) {
     return playlist.name.replace(/[^a-z0-9\- ]/gi, '').replace(/[ ]/gi, '_').toLowerCase() + ".csv";
   }
 }
 
-// This is equivalento to $(document).ready(function() {...}). The function is a callback, called every time the page
-// loads anew.
+// This is equivalento to jquery(document).ready(function() {...}). The function is a callback, called every time
+// the page loads anew.
 // https://api.jquery.com/ready/
 $(function() {
   let [root, hash] = window.location.href.split('#')
+  dict = {}
   if (hash) {
     let params = hash.split('&');
     for (let i = 0; i < params.length; i++) {
       let [k, v] = params[i].split('=');
-      State[k] = v;
+      dict[k] = v;
     }
   }
 
-  if (!State.access_token) { // if we're on the home page
+  if (!dict.access_token) { // if we're on the home page
     $('#loginButton').css('display', 'inline-block')
   } else { // if we were just authorized and got a token
-    React.render(<PlaylistTable access_token={State.access_token} />, playlistsContainer);
+    // variable={value} makes that variable accessible to all React components via this.props.variable!
+    React.render(<PlaylistTable access_token={dict.access_token} />, playlistsContainer);
     window.location = root + "#playlists"
   }
 });
